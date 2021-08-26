514030a8c1a19a3872b5369bf49342b2abfcac7d
Andrés Agüero <89194794+Agr41@users.noreply.github.com>
2021-08-26 13:40:20 -0500

    Closure Colores

--- javas.js
+++ javas.js
@@ -3,52 +3,55 @@ let inputN = document.getElementById("num");
 let butA = document.getElementById("add");
 let butD = document.getElementById("del");
 let butS = document.getElementById("sea");
-let butM = document.getElementById("tod");
 let mostrar = document.getElementById("mos");
-let mostrarT = document.getElementById("dis")
 
-var colores = new Invent("Colores");
+let colores = inventario("Colores");
 
 butA.addEventListener('click', ()=>{
     colores.add(inputV.value, inputN.value);
     console.log("Agregado");
 });
 butD.addEventListener('click', ()=>{
-    colores.borrar(inputV.value);
+    colores.del(inputV.value);
     console.log("Eliminado");
 });
 butS.addEventListener('click', ()=>{
-    mostrar.innerHTML = colores.cantidad(inputV.value);
+    mostrar.innerHTML = colores.num(inputV.value);
     console.log("ok");
 }); 
-/* butM.addEventListener('click', ()=>{
-    //mostrarT.innerHTML = colores.todo(;
-    let ar = colores.todo();
-    ar.forEach(function(elemento, indice){
-        mostrarT.innerHTML += `${indice} ${elemento} hola`
-    })
-    console.log(colores.todo());
-});  */
 
-function Invent (nombre) {
-    this.nombre = nombre;
-    this.articulos = [];
-    
-    this.add = function (nombre, cantidad) {
-        this.articulos[nombre] = cantidad;
-    } 
-    this.borrar = function (nombre) {
-        delete this.articulos[nombre];
-    } 
-    this.cantidad = function (nombre) {
-        return this.articulos[nombre];
-    } 
-    this.getNombre = function () {
-        return this.nombre;
+
+function inventario(nom){
+    let nombre = nom;
+    let elementos = {};
+
+    function add(nombre,cantidad){
+        elementos[nombre]=cantidad;
+    };
+
+    function del(nombre){
+        delete elementos[nombre];
+    };
+
+    function num(nombre){
+        return elementos[nombre];
+    };
+
+    function nomb(){
+        return nombre
+    };
+
+    function todo(){
+        return elementos;
     }
-    this.todo = function () {
-        return this.articulos;
-    }
+
+    return  {
+                add,
+                del,
+                num,
+                nomb,
+                todo
+            }
 }
 
 //colores.todo()

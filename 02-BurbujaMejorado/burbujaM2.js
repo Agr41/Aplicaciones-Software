768e5d571b2387f8546d2b2edb470fd10c9f2783
Andrés Agüero <89194794+Agr41@users.noreply.github.com>
2021-09-13 19:52:03 -0500

    Add files via upload

---
+++ 02-BurbujaMejorado/burbujaM2.js
@@ -0,0 +1,22 @@
+const burbuja = arr => {                                     
+    const longitud = arr.length;                              
+    let ordenado = false;                                    
+    for (let i = 0; i < longitud; i++) {                      
+        ordenado=true;                                        
+        for (let j = 0; j < longitud - 1 - i; j++) {           
+            if (arr[j] < arr[j + 1]) {                         
+                ordenado=false;                               
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];  
+            }
+        }
+        console.log(`Pasada ${i}`);
+        console.log(`${arr}`);    
+        if(ordenado){                      
+            return arr;                  
+        }                                  
+    }
+    
+    return arr;                                            
+};
+const arr = ["Beny", "Andres", "Emilio", "Fernanda", "Jared", "Daniela", "Claudia", "Ivan", "Hernan", "Gabriel"]
+const result = burbuja(arr);
---
+++ 05-ShellDinamico/shellD1.js
@@ -0,0 +1,24 @@
+const shellSort = arr => {
+    const gap = arr.length;
+    let h = 1;
+    while ( h < gap / 3 ) {
+      h = 3 * h + 1;
+    }
+    while ( h >= 1 ) {
+      for ( let i = h; i < gap; i++ ) {
+        for ( let j = i; j >= h && arr[ j ] < arr[ j - h ];  j -= h ) {
+          [ arr[ j ], arr[ j - h ] ] = [ arr[ j - h ], arr[ j ] ];
+        }
+        
+        console.log(`${arr}`);
+      }
+      h = ( h - 1 ) / 3;
+    }
+    
+    return arr;
+  }
+  
+  
+  const arr = ["Beny", "Andres", "Emilio", "Fernanda", "Jared", "Daniela", "Claudia", "Ivan", "Hernan", "Gabriel"]
+  const result = shellSort(arr);
+  
